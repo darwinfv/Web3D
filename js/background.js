6b3cdc5550b0e7846d20dfc4ba4a5976b6521792
Darwin Vaz <dvaz@purdue.edu>
2020-10-27 00:14:49 -0400

    setup and background

---
+++ js/background.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+    el: "body",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 1000,
+    minWidth: 1000,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xffffff,
+    backgroundColor: 0x0,
+    points: 20.00,
+    maxDistance: 10.00,
+    spacing: 15.00
+});
