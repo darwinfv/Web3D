8441037dbe160f7a7b7fb8b67737a3ec1f369d62
Darwin Vaz <dvaz@purdue.edu>
2020-10-30 20:52:33 -0400

    fixed css for nav and sidebars

--- js/background.js
+++ js/background.js
@@ -1,19 +1,19 @@
-VANTA.NET({
-    el: "body",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    color: 0xffffff,
-    backgroundColor: 0x0,
-    points: 20.00,
-    maxDistance: 10.00,
-    spacing: 15.00
-});
+// VANTA.NET({
+//     el: 'body',
+//     mouseControls: true,
+//     touchControls: true,
+//     gyroControls: false,
+//     minHeight: 200.00,
+//     minWidth: 200.00,
+//     scale: 1.00,
+//     scaleMobile: 1.00,
+//     color: 0xffffff,
+//     backgroundColor: 0x0,
+//     points: 20.00,
+//     maxDistance: 10.00,
+//     spacing: 15.00
+// });
 
-let navHeight = document.getElementsByClassName("nav")[0].style.height;
-document.getElementsByClassName("sidebar")[0].style.marginTop = navHeight;
-document.getElementsByClassName("sidebar")[1].style.marginTop = navHeight;
+// let navHeight = window.getComputedStyle(document.getElementsByClassName("navbar")[0]).height;
+// document.getElementsByClassName("sidebar")[0].style.marginTop = navHeight;
+// document.getElementsByClassName("sidebar")[1].style.marginTop = navHeight;
