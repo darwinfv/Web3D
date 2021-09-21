566ad5c8d03474ad989d5e0d5bf72b432551f479
Darwin Vaz <dvaz@purdue.edu>
2021-09-20 21:08:01 -0500

    some lighting adjustments

--- js/setup.js
+++ js/setup.js
@@ -17,10 +17,29 @@ cube.rotation.y += 10;
 cube.rotation.x -= 40;
 // scene.add(cube);
 
-const loader = new THREE.GLTFLoader();
-loader.load('', function(gltf) {
-    scene.add(gltf.scene);
+light = new THREE.AmbientLight(0x404040, 4);
+scene.add(light);
 
+// directionalLight = new THREE.DirectionalLight(0xffffff,1);
+// directionalLight.position.set(0,1,0);
+// directionalLight.castShadow = true;
+// scene.add(directionalLight);
+// light = new THREE.PointLight(0xc4c4c4,10);
+// light.position.set(0,300,500);
+// scene.add(light);
+// light2 = new THREE.PointLight(0xc4c4c4,10);
+// light2.position.set(500,100,0);
+// scene.add(light2);
+// light3 = new THREE.PointLight(0xc4c4c4,10);
+// light3.position.set(0,100,-500);
+// scene.add(light3);
+// light4 = new THREE.PointLight(0xc4c4c4,10);
+// light4.position.set(-500,300,500);
+// scene.add(light4);
+
+const loader = new THREE.GLTFLoader();
+loader.load('https://raw.githubusercontent.com/darwinfv/Web3D/master/models/dough.gltf?token=AFHP3MAQURDAUNWTAPSUJPDBKJ3NG', function(gltf) {
+    scene.add(gltf.scene);
 }, undefined, function(error) {
     console.log(error);
 });
